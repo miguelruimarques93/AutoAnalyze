7899f5e90e77c5ace44995270e90c8d9771510e2
migulorama <migulorama@gmail.com>
2014-04-01 10:03:09 +0100

    Update intermediate representation.

--- src/pt/up/fe/comp/dot/ir/DotGraph.java
+++ src/pt/up/fe/comp/dot/ir/DotGraph.java
@@ -1,5 +1,26 @@
 package pt.up.fe.comp.dot.ir;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class DotGraph {
+    public class Edge {
+        public String destination;
+        Map<String, String> attributes = new HashMap<String, String>();
+        
+        @Override
+        public boolean equals(Object rhs) {
+            if (rhs instanceof Edge) {
+                Edge other = (Edge) rhs;
+                return other.destination.equals(destination) && other.attributes.equals(attributes);
+            }
+            return false;
+        }
+    }
     
+    public Set<String> finalStates = new HashSet<String>();
+    public Map<String, List<Edge>> nodes = new HashMap<String, List<Edge>>();
 }
